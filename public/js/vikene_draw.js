461a206282d34e1feea58f28ac9d3d6d8318b2c5
vikene <vigneashsundar@live.com>
2015-04-18 18:54:22 +0530

    working

--- public/js/vikene_draw.js
+++ public/js/vikene_draw.js
@@ -3,10 +3,9 @@
 $('document').ready( function ()
 {
     var data;
-    var socket = io();
-   
+   var socket = io();
     var canny = document.getElementById("drawer");
-  //  var db = new Firebase('https://shining-fire-3896.firebaseio.com/draw');
+    //var db = new Firebase('https://yooyjava.firebaseio.com/');
     if(canny)
     {
             var context = canny.getContext("2d");
@@ -24,11 +23,8 @@ $('document').ready( function ()
                     ccy = e.pageY - canny.offsetTop;
                     context.moveTo(ccx,ccy);
                     lock_down = true;
-           //        db.child(count).set(ccx+":"+ccy+":"+"0");
-
-                    socket.emit("drawing_onprocess",ccx+":"+ccy+":"+"0");
+                 //  db.child(count).set(ccx+":"+ccy+":"+"0");
                     socket.emit("drawing_start",ccx+":"+ccy+":"+"0");
-
                     count++;
                     
                 
@@ -38,11 +34,8 @@ $('document').ready( function ()
                     {
                         ccx1 = e.pageX - canny.offsetLeft;
                         ccy1 = e.pageY - canny.offsetTop;
-
-                    //   db.child(count).set(ccx1+":"+ccy1+":"+"1");
-                           socket.emit("drawing_onprocess",ccx1+":"+ccy1+":"+"1");
-                      //  db.child(count).set(ccx1+":"+ccy1+":"+"1");
-                              socket.emit("drawing_onprocess",ccx1+":"+ccy1+":"+"1");
+                       // db.child(count).set(ccx1+":"+ccy1+":"+"1");
+                          socket.emit("drawing_onprocess",ccx1+":"+ccy1+":"+"1");
 
                         count++;
                         context.lineTo(ccx1,ccy1);
@@ -50,7 +43,9 @@ $('document').ready( function ()
                         context.stroke();
                         
                          data = context.getImageData(0,0,512,512);
-                
+                var canny2 = document.getElementById("drawer2");
+                var ctx = canny2.getContext("2d");
+                ctx.putImageData(data,0,0);
                        
                         
                     }
@@ -73,11 +68,7 @@ $('document').ready( function ()
                     ccy = e.pageY - canny.offsetTop;
                     context.moveTo(ccx,ccy);
                     lock_down = true;
-
-                 // db.child(count).set(ccx+":"+ccy+":"+"0");
-
-                   //db.child(count).set(ccx+":"+ccy+":"+"0");
-
+                   db.child(count).set(ccx+":"+ccy+":"+"0");
                     count++;
                     
                 
@@ -87,7 +78,7 @@ $('document').ready( function ()
                     {
                         ccx1 = e.pageX - canny.offsetLeft;
                         ccy1 = e.pageY - canny.offsetTop;
-                      //  db.child(count).set(ccx1+":"+ccy1+":"+"1");
+                        db.child(count).set(ccx1+":"+ccy1+":"+"1");
                         count++;
                         context.lineTo(ccx1,ccy1);
                         
@@ -108,11 +99,7 @@ $('document').ready( function ()
      
                 
         });
-
-   /*    function paintit(snapshot)
-
-        function paintit(snapshot)
->>>>>>> parent of 8ddcabe... Socket fine , db down
+       /* function paintit(snapshot)
         {
            var dataa = snapshot.val().split(":");
             if(dataa[2] == 0)
@@ -127,10 +114,9 @@ $('document').ready( function ()
             context.lineTo(dataa[0],dataa[1]);
             context.stroke();
             }
-        }
-<<<<<<< HEAD*/
-      
-        socket.on('paintit',function(snap){
+        }*/
+        
+         socket.on('paintit',function(snap){
            
              var dataa = snap.split(":");
             console.log(snap);
@@ -149,9 +135,8 @@ $('document').ready( function ()
         
         })
         
-
         $('#clear').click(function () {
-           //db.remove();
+           db.remove();
             location.reload();
         });
         
@@ -159,15 +144,9 @@ $('document').ready( function ()
         {
             location.reload();
         }
-/*<<<<<<< HEAD
-      db.on('child_added',paintit);
-       db.on('child_changed',paintit);
-      db.on('child_removed',cleanit);
-=======
-        db.on('child_added',paintit);
+       /* db.on('child_added',paintit);
         db.on('child_changed',paintit);
-        db.on('child_removed',cleanit);
->>>>>>> parent of 8ddcabe... Socket fine , db down*/
+        db.on('child_removed',cleanit);*/
             
     }
     
