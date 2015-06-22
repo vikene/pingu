6d14ae434c822f377580d6a06bb81d09241a0bb6
Vigneash Sundar <vigneashsundar@live.com>
2015-06-22 21:52:28 +0530

    page file counter

--- app.js
+++ app.js
@@ -2,10 +2,44 @@ var express = require('express');
 var Mongo = require('mongodb');
 var monk = require('monk');
 var app = express();
+var fs= require('fs')
+var count = 0;
 
 app.use(express.static(__dirname+'/public'));
 
 var db =monk('mongodb://funnyfork:gmeitw@ds031581.mongolab.com:31581/wingsoffire');
+
+app.get('/dhole/picofday.jpg',function(req,res){
+
+	fs.readFile('picofday.jpg', function(err, data) {
+  if (err) throw err;
+ var collection = db.get("count");
+   
+    collection.find({}, function(err,doc){
+        if(doc[0]!=null)
+        {
+            var dat = parseInt(doc[0].pgcount) +1;
+            
+            var json_obj ={ "pgcount": dat };
+            collection.update({},{"pgcount":dat});
+            console.log(json_obj)
+        }
+        else{
+            collection.insert({"pgcount":0}) //init database
+        }
+    
+    })
+  
+  console.log("Access")
+
+
+   
+    res.send(data);
+  
+ 
+});
+})
+
 
 app.get('/database',function(req,res)
 
